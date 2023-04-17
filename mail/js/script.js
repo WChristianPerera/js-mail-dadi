dfcaa8af801415c0e0f801a4c185dcf5e1896ec0
WChristianPerera <christianperera13@gmail.com>
2023-04-17 19:21:48 +0200

    Add two exercise add work for the dice game

---
+++ mail/js/script.js
@@ -0,0 +1,15 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+/*Utilizzare un prompt per chidere l?email all'utente
+prima di chiedere l'email crea una lista di email nel quale decidi se inserire o no l'email dell utente
+stampare esito dell'email se puo accedere o viceversa */
+
+
+
+let userEmail = prompt('Inserire email');
+console.log(userEmail)
+
+
